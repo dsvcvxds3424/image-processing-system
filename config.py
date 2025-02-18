2eab3d44eb870dc64a13e61642e183ddc358d9cf
dsvcvxds3424 <samedsaf32@outlook.com>
2025-02-18 15:18:35 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
